6cc3a0c1568ec62a703c508aff8cb3ded5c589b4
dhanyajothimani <dhanya.iitd@gmail.com>
2020-06-24 16:17:50 -0400

    Update 03_wiki_author.py
    Updated the code to avoid adding multiple values in a dictionary to a single author (esp. born and died)

--- data/03_wiki_author.py
+++ data/03_wiki_author.py
@@ -9,7 +9,6 @@ from bs4 import BeautifulSoup
 import requests 
 import pandas as pd
 import re
-from dateutil.parser import parse
 import os 
 
 pd.set_option('display.max_rows', None)
@@ -21,9 +20,11 @@ pd.set_option('display.max_colwidth', -1)
 os.chdir("C:\\Users\\Jothimani\\Documents\\GR\\")
 
 #Ref:https://stackoverflow.com/questions/18171739/unicodedecodeerror-when-reading-csv-file-in-pandas-with-python
-author_database = pd.read_csv("author_database.csv",encoding = "ISO-8859-1", engine='python')
-#author_database = pd.read_csv("sample_wiki_scraping.csv",encoding = "ISO-8859-1", engine='python')
-author_database.shape
+author_database1 = pd.read_csv("author_database.csv",encoding = "ISO-8859-1", engine='python')
+print(author_database1.shape)
+
+author_database = author_database1.dropna()
+print(author_database.shape)
 
 def wikipedia_search(subject, url=False):
     if url is False:
@@ -40,22 +41,24 @@ def wikipedia_search(subject, url=False):
 
 def get_author(df):
     
-    born_dict = []
-    died_dict = []
-    award_dict = []
-    lit_movement_dic = []
+    df["born"] = ""
+    df["died"] = ""
+    df["lit_move"] = ""
+    df["awards"] = ""
     
-    for i in df["book_authors"]:
+    for j in df.index:
+        i = df.loc[j]["book_authors"]
         try:
-            print(i)
             soup = wikipedia_search(i)
             infobox = soup.find('table', class_='infobox vcard')
             if infobox is not None:
+                print(i)
                 born = infobox.find('th', text='Born')
                 died = infobox.find('th', text='Died')
                 awards = infobox.find('th', text='Notable awards')
                 litmove = infobox.find('th', text='Literary movement')
             else:
+                print(i)
                 infovbox = soup.find('table', class_='infobox biography vcard') 
                 if infovbox is not None:
                     born = infovbox.find('th', text='Born')
@@ -64,62 +67,52 @@ def get_author(df):
                     litmove = infovbox.find('th', text='Literary movement')
                     
             if born is None:
-                born_dict.append("")
-                print(born)
+                df.at[j, "born"] = ""
+                print("NA")             
             else:
                 born = born.next_sibling
                 born = born.text.strip()
                 born = re.search(r" (\d{4})", born).group(1)
                 print(born)
-                born_dict.append(born)
+                df.at[j,"born"] = born
             
             if died is None:
-                died_dict.append("2020")
-                print(died)
+                df.at[j,"died"] = 2020
+                print("NA")            
             else: 
                 died = died.next_sibling
                 died = died.text.strip()
                 died = re.search(r" (\d{4})", died).group(1)
                 print(died)
-                died_dict.append(died)
+                df.at[j,"died"] = died
                 #https://stackoverflow.com/questions/40121822/extracting-year-from-string-in-python
                 
             if awards is None:
-                award_dict.append("No award")
-                print(awards)
+                df.at[j,"awards"] = "No award"
+                print("No award")
             else: 
                 awards = awards.next_sibling
                 awards = awards.text.strip()
-                award_dict.append(awards)
+                df.at[j,"awards"] = awards
                 print(awards)
         
             if litmove is None:
-                lit_movement_dic.append("Data NA")
-                print(litmove)
+                df.at[j,"lit_move"] = "Data NA"
+                print("Data NA")
             else:
                 litmove = litmove.next_sibling
                 litmove = litmove.text.strip()
-                lit_movement_dic.append(litmove)
+                df.at[j,"lit_move"] = litmove
                 print(litmove)
-    
-                          
-            
                
         except:
-            born_dict.append("")
-            died_dict.append("")
-            award_dict.append("")
-            lit_movement_dic.append("")
+            print(i)
             continue 
-        
-    df["born"] = born_dict
-    df["died"] = died_dict
-    df["notable_award"] = award_dict
-    df["lit_movement"] = lit_movement_dic
+    
+    
+    print(df.shape)
     return (df)
 
-#author_database1 = author_database[:10]
-auth_db = get_author(author_database)
-auth_db.to_csv("author_database_updated.csv")
 
-
+auth_db1 = get_author(author_database)
+auth_db1.to_csv("author_database_updated.csv")
